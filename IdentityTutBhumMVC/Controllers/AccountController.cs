0b682664704120136c8c46f6cfca554f015a92a3
Team ROSPL <vkdevrospl@hotmail.com>
2022-06-08 16:43:03 +0530

    login functioanlity added

--- IdentityTutBhumMVC/Controllers/AccountController.cs
+++ IdentityTutBhumMVC/Controllers/AccountController.cs
@@ -1,10 +1,20 @@
-﻿using IdentityTutBhumMVC.Models.ViewModel;
+﻿using IdentityTutBhumMVC.Models;
+using IdentityTutBhumMVC.Models.ViewModel;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityTutBhumMVC.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager,SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
         public IActionResult Index()
         {
             return View();
@@ -14,6 +24,31 @@ namespace IdentityTutBhumMVC.Controllers
         {
             RegisterVM registerVM = new RegisterVM();
             return View(registerVM);
+        }
+        [HttpPost]
+        //[IgnoreAntiforgeryToken]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                var userdata=new ApplicationUser { UserName=model.Email,Email=model.Email,AddtionalName=model.AddtionlName };
+                var result=await userManager.CreateAsync(userdata,model.Password);
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(userdata, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+                AddErrors(result);
+            }
+            return View(model);
+        }
+        private void AddErrors(IdentityResult result)
+        {
+            foreach(var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
         }
 
 
