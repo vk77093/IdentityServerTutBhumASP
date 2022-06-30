31158ef284112c4da6f40f5edb810949e95e7ea2
Team ROSPL <vkdevrospl@hotmail.com>
2022-06-30 10:47:47 +0530

    added claims store and View Models

---
+++ IdentityTutBhumMVC/Models/ViewModel/ClaimsVm/ClaimsStores.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Claims;
+
+namespace IdentityTutBhumMVC.Models.ViewModel.ClaimsVm
+{
+    public static class ClaimsStores
+    {
+        public static List<Claim> ClaimsList = new List<Claim>()
+        {
+            new Claim("Create","Create"),
+            new Claim("Edit","Edit"),
+            new Claim("Delete","Delete"),
+        };
+    }
+}
---
+++ IdentityTutBhumMVC/Models/ViewModel/ClaimsVm/UserClaimsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Claims;
+
+namespace IdentityTutBhumMVC.Models.ViewModel.ClaimsVm
+{
+    public class UserClaimsViewModel
+    {
+        public UserClaimsViewModel()
+        {
+            Claims = new List<UserClaims>();
+        }
+        public string? UserId { get; set; }
+        public List<UserClaims> Claims { get; set; }
+    }
+    public class UserClaims{
+        public string? ClaimType { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
